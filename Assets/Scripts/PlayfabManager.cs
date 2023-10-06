83099595d59523ec289518a6eabc9295dee6aa90
DAPHNE GOH <0107daph@gmail.com>
2023-10-06 18:41:50 +0800

    send() and get() leaderboard, display user score in leaderboard

--- Assets/Scripts/PlayfabManager.cs
+++ Assets/Scripts/PlayfabManager.cs
@@ -1,11 +1,17 @@
+using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
 
 public class PlayfabManager : MonoBehaviour
 {
+    [Header("Leaderboard")]
+    public GameObject rowPrefab;
+    public Transform rowsParent;
 
     void Start()
     {
@@ -66,9 +72,19 @@ public class PlayfabManager : MonoBehaviour
 
     void OnLeaderboardGet(GetLeaderboardResult result)
     {
+        foreach (Transform item in rowsParent)
+        {
+            Destroy(item.gameObject);
+        }
         foreach (var item in result.Leaderboard)
         {
-            Debug.Log(item.Position +" " +item.PlayFabId+" "+ item.StatValue);
+            GameObject newGo = Instantiate(rowPrefab, rowsParent);
+            TMP_Text[] texts = newGo.GetComponentsInChildren<TMP_Text>();
+            texts[0].text = (item.Position+1).ToString();
+            texts[1].text = item.PlayFabId;
+            texts[2].text = item.StatValue.ToString();
+
+            Debug.Log(string.Format("Place: {0} | ID: {1} | Value: {2}",item.Position, item.PlayFabId, item.StatValue));
         }
     }
     
